43757a2b6b81edaddd4b1c589273bf10d455a715
Solo800606 <61974103+Solo800606@users.noreply.github.com>
2020-04-04 14:25:43 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,4 +1,3 @@
-﻿# Запуск pytest --language=es test_items.py
 import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -6,9 +5,10 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_presence_of_the_Add_to_basket_button(browser):
     browser.get(link)
     time.sleep(30)
-    browser.find_elements_by_css_selector("language.form-control")
+    button = None
+    try:
+        button = browser.find_element_by_class_name('btn-add-to-basket')
+    except NoSuchElementException:
+        pass
+    assert button!=None, 'Should be button add to basket'
     
-    text_elt = browser.find_element_by_css_selector(".btn-add-to-basket")
-    text_basket = text_elt.text
-    assert "Añadir al carrito" == text_basket
-
